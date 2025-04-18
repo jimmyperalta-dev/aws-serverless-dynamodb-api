6570d8f3b2991027cda3b364c3c88debae06e730
Jimmy Peralta <aridio.peralta@gmail.com>
2025-04-18 05:51:09 -0400

    Initial implementation of serverless Notes API

---
+++ src/functions/listNotes.js
@@ -0,0 +1,16 @@
+// src/functions/listNotes.js
+const dynamodb = require('../lib/dynamodb');
+const response = require('../lib/responseUtil');
+
+exports.handler = async () => {
+  try {
+    // Get all notes from DynamoDB
+    const notes = await dynamodb.listItems();
+    
+    // Return success response
+    return response.success({ notes });
+  } catch (error) {
+    console.error('Error listing notes:', error);
+    return response.serverError('Failed to list notes');
+  }
+};
